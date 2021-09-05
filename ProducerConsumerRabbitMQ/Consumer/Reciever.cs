6aac81396792ddf57bc6bb3df2085bc1ad71439a
Achyuth <guduri.kumar@wipro.com>
2021-09-05 12:10:13 +0530

    Add project files.

---
+++ ProducerConsumerRabbitMQ/Consumer/Reciever.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Reciever
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (ma, e) =>
+                {
+                    var body = e.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Recieved Message {0}", message);
+                };
+                channel.BasicConsume("BasicTest", true, consumer);
+                Console.WriteLine("press enter to exit the consumer");
+                Console.ReadKey();
+            }
+        }
+    }
+}
